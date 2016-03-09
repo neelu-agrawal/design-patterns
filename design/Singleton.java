e0e24117840a656aa7eda590726b06e8a60b3b7b
neeluagrawal <neelu.agrawal16@gmail.com>
2016-03-09 23:44:53 +0530

    Adding a test class to repo

---
+++ design/Singleton.java
@@ -0,0 +1,13 @@
+package design;
+
+/**
+ * Created by neeluagrawal on 09-03-2016.
+ */
+public class Singleton {
+
+    public static void main(String [] args){
+        System.out.println("singletone test");
+    }
+
+
+}
